b149695aeb9860e88d7550d6d3e252b912010a18
Jems <jamessantos996@gmail.com>
2023-01-21 14:28:33 +0800

    added padding on ChatInput

--- src/components/ChatInput.jsx
+++ src/components/ChatInput.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ChatInput = () => {
     return (
-        <form className="flex flex-col items-center gap-3 sm:flex-row">
+        <form className="flex flex-col items-center gap-3 px-3 py-2 sm:flex-row">
             <div className="flex">
                 <button className="h-9 w-9">
                     <i class="fa-solid fa-circle-plus text-base text-blue-500"></i>
